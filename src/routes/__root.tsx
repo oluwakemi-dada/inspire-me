727990427b4cf65f170eef66055e970264f26f05
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-08-01 15:44:24 +0100

    feat: header and app main layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import {
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { QueryClient } from '@tanstack/react-query';
+import Header from '@/components/Header';
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -23,11 +24,20 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       },
     ],
   }),
-  component: () => (
-    <>
-      <HeadContent />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: RootLayout,
 });
+
+function RootLayout() {
+  return (
+    <div className='min-h-screen bg-gray-100 flex flex-col'>
+      <HeadContent />
+      <Header />
+      <main className='flex justify-center p-6'>
+        <div className='w-full max-w-4xl bg-white rounded-2xl shadow-lg p-8'>
+          <Outlet />
+        </div>
+      </main>
+      <TanStackRouterDevtools />
+    </div>
+  );
+}
